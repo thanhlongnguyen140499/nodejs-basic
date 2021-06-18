0f38e3f7050f4e8eb6d4a2406c66e3040239d12f
thanhlongnguyen140499 <thanhlongnguyen140499@gmail.com>
2021-06-18 20:53:27 +0700

    fake data create, show list user

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const exphbs = require('express-handlebars');
 
 const app = express();
 
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
 // ! configure express-handlebars
 app.engine('hbs', exphbs({
     defaultLayout: 'main',
@@ -15,18 +20,42 @@ app.engine('hbs', exphbs({
 app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
-    res.render('home');
+    res.render('home', {
+        name: 'AAA'
+    });
 });
+
+var users = [
+    { id: 1, name: 'Edward'},
+    { id: 2, name: 'Leonard'},
+    { id: 3, name: 'Frank'},  
+]
 
 app.get('/users', (req, res) => {
     res.render('users/users', {
-        users: [
-            { id: 1, name: 'Edward'},
-            { id: 2, name: 'Leonard'},
-            { id: 3, name: 'Frank'},
-        ]
+        users: users,
     });
 });
+
+app.get('/users/search', (req, res) => {
+    var q = req.query.name
+    var matchedUsers = users.filter(function(user) {
+        return user.name.toLowerCase().indexOf(q) !== -1;
+    })
+    res.render('users/users', {
+        users: matchedUsers,
+    })
+})
+
+app.get('/users/create', (req, res) => {
+    res.render('users/create')
+})
+
+app.post('/users/create', (req, res) => {
+    // console.log(req.body)
+    users.push(req.body)
+    res.redirect('/users')
+})
 
 app.listen(3000, () => {
     console.log('The web server has started on port 3000');
